acc249131effd3a5ee07d5df8febb64b76973b25
Florian Barthel <florianbarthel@yahoo.de>
2021-02-01 12:12:35 +0100

    update api

--- application/api/api.py
+++ application/api/api.py
@@ -1,57 +1,77 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify
 from PIL import Image
-import os, io, sys
+import io, sys
 import numpy as np
 import base64
 sys.path.append("../../")
+CUDA_VISIBLE_DEVICES = ""
+
 import dnnlib.tflib as tflib
 import training.misc as misc
 from flask_cors import CORS
+import tensorflow as tf
+
 
 app = Flask(__name__)
 CORS(app)
 
+session = tflib.create_session(None, force_as_default=True)
 
-class Graph:
-    def __init__(self, network_pkl):
-        tflib.init_tf()
-        _G, _D, Gs = misc.load_pkl(network_pkl)
-        self.network_pkl = network_pkl
-        self.Gs = Gs
-
-    def generate_image(self, onehot, seed):
-        tflib.init_tf()
-        _G, _D, Gs = misc.load_pkl(self.network_pkl)
-        self.Gs = Gs
-        if seed is -1:
-            latents = np.random.normal(0, 1, [1, self.Gs.input_shape[1]])
-        else:
-            latents = np.random.RandomState(seed).normal(0, 1, [1, self.Gs.input_shape[1]])
-        image = self.Gs.get_output_for(latents, onehot, is_validation=True, randomize_noise=False, truncation_psi_val=0.7)
-        return tflib.convert_images_to_uint8(image, nchw_to_nhwc=True).eval()[0]
+latent_placeholder = tf.placeholder(tf.float32, shape=(None, 512))
+label_placeholder = tf.placeholder(tf.float32, shape=(None, 120))
 
 
-graph = Graph('../../results/00021-stylegan2-all_cars_all_labels_256-2gpu-config-f/network-snapshot-004112.pkl')
+_G, _D, Gs = misc.load_pkl('../../results/00030-stylegan2-cars_v1_512-2gpu-config-f/network-snapshot-003369.pkl')
+gen_image = Gs.get_output_for(latent_placeholder, label_placeholder, is_validation=True, randomize_noise=True,
+                          truncation_psi_val=1.0)
+
+
+def run(label, seed):
+    if seed is -1:
+        latent = np.random.normal(0, 1, [1, Gs.input_shape[1]]).astype(np.float32)
+    else:
+        latent = np.random.RandomState(seed).normal(0, 1, [1, Gs.input_shape[1]]).astype(np.float32)
+
+    with tf.device('/cpu:0'):
+        image = session.run(gen_image, feed_dict={latent_placeholder: latent, label_placeholder: label})[0]
+        image = np.transpose(image, [1, 2, 0])
+        image = image * 127.5 + 127.5
+        image = np.clip(image, 0, 255).astype(np.uint8)
+        return image
 
 
 @app.route('/image', methods=['GET', 'POST'])
 def image():
-    data = str(request.data)[1:].replace('\'', '').split(',')
-    print(data)
-    manufacturer = int(data[0])
-    model = int(data[1])
-    color = int(data[2])
-    seed = int(data[3])
+    data = request.get_json()
+    # print(data)
+    payload = data['payload']
+    print(payload)
+    manufacturer = int(payload['manufacturer'])
+    model = int(payload['model'])
+    color = int(payload['color'])
+    body = int(payload['body'])
+    rotation = int(payload['rotation'])
+    ratio = int(payload['ratio'])
+    seed = int(payload['seed'])
 
-    onehot = np.zeros((1, 97), dtype=np.float)
-    onehot[0, model] = 1.0
-    onehot[0, color + 67] = 1.0
-    onehot[0, 67 + 12 + manufacturer] = 1.0
+    onehot = np.zeros((1, 120), dtype=np.float)
+    if model >= 0:
+        onehot[0, model] = 1.0
+    if color >= 0:
+        onehot[0, color + 67] = 1.0
+    if manufacturer >= 0:
+        onehot[0, 67 + 12 + manufacturer] = 1.0
+    if body >= 0:
+        onehot[0, 67 + 12 + 18 + body] = 1.0
+    if rotation >= 0:
+        onehot[0, 67 + 12 + 18 + 10 + rotation] = 1.0
+    onehot[0, 67 + 12 + 18 + 10 + 8 + ratio] = 1.0
 
-    image = graph.generate_image(onehot, seed)
-    img = image[48:-48,:]
-    img = Image.fromarray(img.astype("uint8"))
-    img = img.resize((256 * 3, 160 * 3))
+    # print(onehot)
+    image = run(onehot, seed)
+    # img = image[48:-48,:]
+    img = Image.fromarray(image)
+    img = img.resize((512, 512))
 
     rawBytes = io.BytesIO()
     img.save(rawBytes, "JPEG")
@@ -67,7 +87,7 @@ def index():
 
 @app.after_request
 def after_request(response):
-    print("log: setting cors", file=sys.stderr)
+    # print("log: setting cors", file=sys.stderr)
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
@@ -75,4 +95,5 @@ def after_request(response):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=False)
+    app.run(host='0.0.0.0', debug=False)
